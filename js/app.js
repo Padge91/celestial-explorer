677cda0ad9d645e9e76a69b90ff79fa3d4213856
Nick <nickpadge@gmail.com>
2017-09-04 20:20:44 -0400

    Updated code to use particles, so can effectively render all stars in database.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,118 @@
 
+
+var starColorRanges = [-0.33, -0.17, 0.15, 0.44, 0.68, 1.15, 1.64];
+var starColorValues = ["#00BFFF", "#87CEFA", "#AFEEEE", "#EEE8AA", "#FFD700", "#FF8C00", "#FF4500"];
+
+
+
 //set up standard scene objects
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 var renderer = new THREE.WebGLRenderer();
+var particles = new THREE.Geometry();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//camera
-camera.position.z = 5;
 
+//calculate actual color of the star
+function calculateColor(colorIndex){
+	for (var i = 0; i < starColorRanges.length; i++){
+		if (colorIndex < starColorRanges[i]){
+			return starColorValues[i];
+		}
+	}
+	return starColorValues[starColorValues.length-1];
+}
+
+
+//animate
 function animate() {
 	requestAnimationFrame( animate );
 	controls.update();
 }
 
+
+//render the scene
 function render() {
 	renderer.render( scene, camera );
-	stats.update();
 }
 
+
+//build camera controls
 function buildControls(){
-				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-				camera.position.z = 500;
-				controls = new THREE.TrackballControls( camera );
-				controls.rotateSpeed = 1.0;
-				controls.zoomSpeed = 1.2;
-				controls.panSpeed = 0.8;
-				controls.noZoom = false;
-				controls.noPan = false;
-				controls.staticMoving = true;
-				controls.dynamicDampingFactor = 0.3;
-				controls.keys = [ 65, 83, 68 ];
-				controls.addEventListener( 'change', render );
-
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, .1, 10000000000000 );
+	camera.position.z = 5;
+	controls = new THREE.TrackballControls( camera );
+	controls.rotateSpeed = 1.0;
+	controls.zoomSpeed = 1.2;
+	controls.panSpeed = 0.8;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	controls.keys = [ 65, 83, 68 ];
+	controls.addEventListener( 'change', render );
 }
 
-buildControls();
-animate();
 
 //create gemoetrical objects from objects in data
 function loadObjectsInScene(objectsArray){
 	log("Loading objects");
+	
 	for (var i = 0; i < objectsArray.length; i++){
 	        var o = objectsArray[i];
-		// formula for converting to galactic coordinates
-		var xg =  -(0.0550*o.x) -(0.8732*o.y) - (0.4839*o.z)
-		var yg =  (0.4940*o.x) - (0.4449*o.y) + (0.7470*o.z)
-		var zg = -(0.8677*o.x) - (0.1979*o.y) + (0.4560*o.z)
-		log(o);
-		
-	        var geometry = new THREE.SphereGeometry( 1);
-	        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	        var sphere = new THREE.Mesh( geometry, material );
-	        scene.add( sphere );
-	        sphere.position.set(xg,yg,zg);
-		if (i >= 1000){
-			break;
+
+		if (o.dist >= 10000000 || o.dist < 0){
+			continue;
 		}
+			
+		// add element
+		var x = o.x;
+		var y = o.y;
+		var z = o.z;
+		var color = calculateColor(o.ci);
+
+	        var particle = new THREE.Vector3(x, y, z);
+
+		particles.vertices.push(particle);
+		particles.colors.push(new THREE.Color(color));
+		
 	}
+
+
+	var material = new THREE.PointsMaterial({
+		vertexColors: THREE.VertexColors,
+                size: .5,
+		map: THREE.ImageUtils.loadTexture("/images/particle.png"),
+      		blending: THREE.AdditiveBlending,
+      		transparent: true,
+		depthWrite: false
+        });
+	
+	var pointCloud = new THREE.Points(particles, material);
+	scene.add(pointCloud);
+	
+	// create the particle system
 	log("Finished loading objects.");
 }
 
-var intervalId = setInterval(function(){
-	if (window.celestialData){
-		clearInterval(intervalId);
-		loadObjectsInScene(window.celestialData);
-	}
-}, 1000);
+
+//load data nd objects
+function loadDataAndObjects(){
+	var intervalId = setInterval(function(){
+		if (window.celestialData){
+			clearInterval(intervalId);
+			loadObjectsInScene(window.celestialData);
+		}
+	}, 1000);
+}
+
+
+//main method
+function main(){
+        buildControls();
+	loadDataAndObjects();
+	animate();
+}
+
+
+main();
